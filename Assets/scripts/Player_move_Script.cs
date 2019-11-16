31d80099b0fcc9e4b668367930491f583a862058
623623 <34660886+623623@users.noreply.github.com>
2019-11-16 20:46:10 +0200

    game
    game

---
+++ Assets/scripts/Player_move_Script.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move_Script : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    SpriteRenderer sp;
+    Animator anim;
+    public float speed;
+    public float jumpVector = 1.0f;
+    float moveInput;
+
+    public float isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        //код кнопок лево и право+анимации
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            
+            anim.Play("Run_Rogue");
+            sp.flipX = false;
+        }
+
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            
+            anim.Play("Run_Rogue");
+            sp.flipX = true;
+        }
+        else
+        {
+            anim.Play("Idle_Rogue");
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpVector;
+            anim.Play("Jump_Rogue");
+
+        }
+        //код бега игрока
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        
+
+
+    }
+    
+    
+
+}
+
+
